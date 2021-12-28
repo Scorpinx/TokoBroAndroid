863fe6d3af2f301b5d3f94214d85557e835f9105
scorpinx <zae1810@gmail.com>
2021-12-29 01:05:19 +0700

    First init

---
+++ app/src/main/java/com/example/tokobro/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tokobro
+
+import git remote add origin https://github.com/Scorpinx/TokoBroAndroid.gitandroidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
